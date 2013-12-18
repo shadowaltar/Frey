3b01e49ab4994be0cab2b652c09b7191cf7dd368
shadowaltar <shadowaltar@gmail.com>
2013-12-19 01:48:24 +0800

    Add ignore list, add first ranking stgy

---
+++ Frey/Strategies/SharpeRankingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Automata.Strategies
+{
+    public class SharpeRankingStrategy : Strategy
+    {
+        public SharpeRankingStrategy()
+        {
+            StrategyType = StrategyType.SecuritySelection;
+        }
+    }
+}
