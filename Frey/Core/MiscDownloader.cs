d822810076704c82eecacab798294445392a197c
shadowaltar <shadowaltar@gmail.com>
2014-01-07 18:36:30 +0800

    add a MiscDownloader; testing the new metro ui

---
+++ Frey/Core/MiscDownloader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace Automata.Core
+{
+    public static class MiscDownloader
+    {
+        public static void Download(params string[] urls)
+        {
+            using (var wc = new WebClient())
+            {
+                foreach (var url in urls)
+                {
+                    var fileName = url.Split('/').Last();
+                    var filePath = Path.Combine(Context.StaticDataFileDirectory, fileName);
+
+                    System.Console.WriteLine("START "+filePath);
+                    wc.DownloadFile(url, filePath);
+                    System.Console.WriteLine("DONE");
+                }
+            }
+        }
+    }
+}
