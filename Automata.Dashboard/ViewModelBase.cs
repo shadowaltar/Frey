5251f63eb0f100da3d8775ee2b919e1cdbca496c
shadowaltar <shadowaltar@gmail.com>
2014-05-21 20:43:57 +0800

    more on the collar twin win #3

---
+++ Automata.Dashboard/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using Caliburn.Micro;
+
+namespace Automata.Dashboard
+{
+    public class ViewModelBase : Screen
+    {
+        public bool SetNotify<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(target, value))
+            {
+                target = value;
+                NotifyOfPropertyChange(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
