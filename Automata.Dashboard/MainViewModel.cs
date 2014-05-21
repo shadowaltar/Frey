7e454a4eb401d7dc62b765b3b6e4e22d81c54b35
shadowaltar <shadowaltar@gmail.com>
2014-05-21 20:14:41 +0800

    more on collar twin win #2

--- Automata.Dashboard/MainViewModel.cs
+++ Automata.Dashboard/MainViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,48 +9,18 @@ namespace Automata.Dashboard
 {
     public class MainViewModel : IMainViewModel
     {
-        //      private MonteCarloGenerator mc = new MonteCarloGenerator();
+        private readonly Dictionary<double, double> resultChangingS0 = new Dictionary<double, double>();
+        private readonly Dictionary<double, double> resultChangingVol = new Dictionary<double, double>();
 
-        private Dictionary<double, double> resultValues = new Dictionary<double, double>();
+        private int totalIterations = 500000;
 
-        public void Go()
+        private double riskFreeRate = 0.005809777;
+
+        public void CalculateWithChangingStockPrice()
         {
-            for (int i = 80; i <= 125; i++)
-            {
-                GoMultiple(i);
-            }
-
-            foreach (var resultValue in resultValues.OrderBy(rv => rv.Key))
-            {
-                Console.WriteLine(resultValue.Key + " : " + resultValue.Value);
-            }
-        }
-
-        //private double CalculateTrunk(double s0,
-        //    double strike, double capStrike, double knockOutBarrier,
-        //    double knockedOutFloor,
-        //    double riskFreeRate,
-        //    double priceStdev, double tenor, int steps, int iteration)
-        //{
-        //    var result = 0d;
-        //    for (int i = 0; i < iteration; i++)
-        //    {
-        //        //sum += CalculateCall(s0, strike, rf, priceVol, tenor, steps);
-        //        result += CalculateTwinWinCollar(s0, strike, capStrike, barrier, floor,
-        //            rf, priceVol, tenor, steps);
-        //    }
-        //    return result / iteration;
-        //}
-
-        public void GoMultiple(double s0)
-        {
-            var totalIterations = 500000;
-
             var steps = 250;
             var tenor = 1d;
-            var rf = 0.0112;
             var priceVol = 0.0551084226;
-
             var strike = 100d;
 
             var capStrike = 130d;
@@ -61,83 +30,101 @@ namespace Automata.Dashboard
             var deltaS = 0.1;
 
             var trunkCount = 8;
-            var s0s = new List<double> { s0, s0 + deltaS, s0 + 2 * deltaS };
 
-            using (ReportTime.StartWithMessage("{0} -------- stock " + s0))
+            var startingS0 = 80;
+            var endingS0 = 130;
+            using (ReportTime.StartWithMessage("TOTAL TIME ELAPSED {0} (Delta & Gamma)."))
             {
-                foreach (var s in s0s)
+                for (int s0 = startingS0; s0 <= endingS0; s0++)
                 {
-                    var trunks = new double[trunkCount];
-                    var iteration = totalIterations / trunkCount;
+                    var s0s = new List<double> { s0, s0 + deltaS, s0 + 2 * deltaS };
 
-                    var results = Parallel.For(0, trunkCount, j =>
+                    using (ReportTime.StartWithMessage("{0} -------- stock " + s0))
                     {
-                        MonteCarloGenerator mc = new MonteCarloGenerator();
-                        var result = 0d;
-                        for (int i = 0; i < iteration; i++)
+                        foreach (var s in s0s)
                         {
-                            result += CalculateTwinWinCollar(s, strike, capStrike, barrier, floor,
-                                rf, priceVol, tenor, steps, mc);
+                            var trunks = new double[trunkCount];
+                            var iteration = totalIterations / trunkCount;
+
+                            Parallel.For(0, trunkCount, j =>
+                            {
+                                var mc = new MonteCarloGenerator();
+                                var result = 0d;
+                                for (int i = 0; i < iteration; i++)
+                                {
+                                    result += CalculateTwinWinCollar(s, strike, capStrike, barrier, floor,
+                                        riskFreeRate, priceVol, tenor, steps, mc);
+                                }
+                                trunks[j] = result / iteration;
+                            });
+
+                            resultChangingS0[s] = trunks.Average();
                         }
-                        trunks[j] = result / iteration;
-                    });
-
-                    resultValues[s] = trunks.Average();
+                    }
                 }
-
-                //s0 += 0.1;
-
-                //for (int i = 0; i < loops; i++)
-                //{
-                //    //sum += CalculateCall(s0, strike, rf, priceVol, tenor, steps);
-                //    value2 += CalculateTwinWinCollar(s0, strike, capStrike, barrier, floor,
-                //        rf, priceVol, tenor, steps);
-                //}
-                //value2 /= loops;
-                //Console.WriteLine("VALUE2: " + value2);
-
-                //s0 += 0.1;
-
-                //for (int i = 0; i < loops; i++)
-                //{
-                //    //sum += CalculateCall(s0, strike, rf, priceVol, tenor, steps);
-                //    value3 += CalculateTwinWinCollar(s0, strike, capStrike, barrier, floor,
-                //        rf, priceVol, tenor, steps);
-                //}
-                //value3 /= loops;
-                //Console.WriteLine("VALUE3: " + value3);
-
-
-                //var delta1 = (value2 - value1) / 0.1;
-                //var delta2 = (value3 - value2) / 0.1;
-                //var gamma = (delta2 - delta1) / 0.1;
-
-                //Console.WriteLine("DELTA1: " + delta1);
-                //Console.WriteLine("DELTA2: " + delta2);
-                //Console.WriteLine("GAMMA1: " + gamma);
             }
+            foreach (var resultValue in resultChangingS0.OrderBy(rv => rv.Key))
+            {
+                Console.WriteLine(resultValue.Key + " : " + resultValue.Value);
+            }
+        }
 
-            //var fourTrunks = new double[4];
-            //var subLoops = loops / 4;
-            //Parallel.ForEach(fourTrunks, trunk =>
-            //{
-            //    var newMc = new MonteCarloGenerator();
-            //    var s = 0d;
-            //    for (int i = 0; i < subLoops; i++)
-            //    {
-            //        //sum += CalculateCall(s0, strike, rf, priceVol, tenor, steps);
-            //        s += CalculateTwinWinCollar(s0, strike, capStrike, barrier, floor,
-            //            rf, priceVol, tenor, steps, newMc);
-            //    }
-            //    s /= subLoops;
-            //    Console.WriteLine(s);
-            //});
+        public void CalculateWithChangingVolatility()
+        {
+            var s0 = 100d;
+            var steps = 250;
+            var tenor = 1d;
+
+            var strike = 100d;
+
+            var capStrike = 130d;
+            var barrier = 90d;
+            var floor = 90d;
+
+            var trunkCount = 8;
+
+            var vols = new List<double> { 0.03,
+                    0.04,
+                    0.0422225, 0.0522225,
+                    0.0551084226, 0.0651084226,
+                    0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35
+                };
+            using (ReportTime.StartWithMessage("TOTAL TIME ELAPSED {0} (Vega)."))
+            {
+                foreach (var vol in vols)
+                {
+                    using (ReportTime.StartWithMessage("{0} -------- vol " + vol))
+                    {
+                        var trunks = new double[trunkCount];
+                        var iteration = totalIterations / trunkCount;
+
+                        double vol1 = vol;
+                        Parallel.For(0, trunkCount, j =>
+                        {
+                            var mc = new MonteCarloGenerator();
+                            var result = 0d;
+                            for (int i = 0; i < iteration; i++)
+                            {
+                                result += CalculateTwinWinCollar(s0, strike, capStrike, barrier, floor,
+                                    riskFreeRate, vol1, tenor, steps, mc);
+                            }
+                            trunks[j] = result / iteration;
+                        });
+
+                        resultChangingVol[vol] = trunks.Average();
+                    }
+                }
+            }
+            foreach (var resultValue in resultChangingVol.OrderBy(rv => rv.Key))
+            {
+                Console.WriteLine(resultValue.Key + " : " + resultValue.Value);
+            }
         }
 
         public double CalculateTwinWinCollar(double s0,
             double strike, double capStrike, double knockOutBarrier,
             double knockedOutFloor,
-            double riskFreeRate,
+            double rf,
             double priceStdev, double tenor, int steps,
             MonteCarloGenerator mc)
         {
@@ -150,7 +137,7 @@ namespace Automata.Dashboard
             for (int i = 0; i < steps; i++)
             {
                 double drift;
-                mc.NextPriceWithAntithetic(st, stAnti, riskFreeRate, priceStdev, dt, out drift, out st, out stAnti);
+                mc.NextPriceWithAntithetic(st, stAnti, rf, priceStdev, dt, out drift, out st, out stAnti);
                 if (!knockedOut1 && st <= knockOutBarrier)
                 {
                     knockedOut1 = true;
@@ -169,7 +156,7 @@ namespace Automata.Dashboard
                 : Math.Min(Math.Abs(stAnti - strike), capStrike - strike);
 
             return (price1 + price2) / 2
-                * Math.Exp(-riskFreeRate * tenor);
+                * Math.Exp(-rf * tenor);
         }
 
         //public double CalculateCall(double s0, double strike, double riskFreeRate,
