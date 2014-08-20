b9f678dd975883907d4375e2209b5b875981f9bb
shadowaltar <shadowaltar@gmail.com>
2014-08-20 17:51:59 +0800

    [TRADING ALPHA] New

---
+++ Trading.Alpha/Trading.Common/ViewModels/ViewModelBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Caliburn.Micro;
+using Ninject;
+using ILog = log4net.ILog;
+using LogManager = log4net.LogManager;
+
+
+namespace Trading.Common.ViewModels
+{
+    public class ViewModelBase : Screen
+    {
+        protected static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Inject]
+        public virtual IEventAggregator EventAggregator { get; set; }
+
+        public bool SetNotify<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(target, value))
+            {
+                target = value;
+                NotifyOfPropertyChange(propertyName);
+                return true;
+            }
+            return false;
+        }
+
+        public bool SetNotify<T>(ref T target, T value, System.Action onChangedCallback, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(target, value))
+            {
+                target = value;
+                NotifyOfPropertyChange(propertyName);
+                onChangedCallback();
+                return true;
+            }
+            return false;
+        }
+    }
+}
