0c932afad00dfd0aefa0539e270e59bec60e7a81
shadowaltar <shadowaltar@gmail.com>
2014-11-07 18:29:52 +0800

    minor change

--- Trading.Alpha/Trading.DataDownload/YahooDailyPriceWorker.cs
+++ Trading.Alpha/Trading.DataDownload/YahooDailyPriceWorker.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Reflection;
-using System.Web;
 using log4net;
 
 namespace Trading.DataDownload
@@ -14,30 +13,31 @@ namespace Trading.DataDownload
         private const string DownloadUrl
             = @"http://real-chart.finance.yahoo.com/table.csv?s={0}&a={1}&b={2}&c={3}&d={4}&e={5}&f={6}&g=d&ignore=.csv";
 
-        public void Download(string yahooStockSymbol, DateTime from, DateTime to, string saveFilePath)
+        public void Download(string yahooStockSymbol, DateTime from, DateTime to, string saveFilePath, bool overwrite = true)
         {
             var url = string.Format(DownloadUrl, yahooStockSymbol,
                 from.Month - 1, from.Day, from.Year,
                 to.Month - 1, to.Day, to.Year);
             var req = WebRequest.Create(url);
             req.Timeout = 20 * 1000;
+
+            if (!overwrite && File.Exists(saveFilePath))
+                return;
+
             try
             {
                 using (var response = req.GetResponse())
+                using (var responseStream = response.GetResponseStream())
                 {
-                    using (var responseStream = response.GetResponseStream())
+                    if (responseStream == null) return;
+
+                    using (var reader = new StreamReader(responseStream))
                     {
-                        if (responseStream != null)
-                        {
-                            using (var reader = new StreamReader(responseStream))
-                            {
-                                var content = reader.ReadToEnd();
-                                if (File.Exists(saveFilePath))
-                                    File.Delete(saveFilePath);
-                                File.WriteAllText(saveFilePath, content);
-                                Log.InfoFormat("Saved {0} ({1} - {2}) to {3}", yahooStockSymbol, from, to, saveFilePath);
-                            }
-                        }
+                        var content = reader.ReadToEnd();
+                        if (File.Exists(saveFilePath))
+                            File.Delete(saveFilePath);
+                        File.WriteAllText(saveFilePath, content);
+                        Log.InfoFormat("Saved {0} ({1} - {2}) to {3}", yahooStockSymbol, @from, to, saveFilePath);
                     }
                 }
             }
