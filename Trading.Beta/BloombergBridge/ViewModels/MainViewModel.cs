cef90dfcf92d7d45db20e82ac20a140271e2db27
shadowaltar <shadowaltar@gmail.com>
2014-11-14 18:02:48 +0800

    work on bbg data retriever

--- Trading.Beta/BloombergBridge/ViewModels/MainViewModel.cs
+++ Trading.Beta/BloombergBridge/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BloombergBridge.Data;
+using Caliburn.Micro;
 using PropertyChanged;
 using Trading.Common.Data;
+using Trading.Common.Entities;
 using Trading.Common.Utils;
 using Trading.Common.ViewModels;
 
@@ -13,6 +15,40 @@ namespace BloombergBridge.ViewModels
 
         public MainViewModel(IDataAccessFactory<Access> dataAccessFactory)
             : base(dataAccessFactory)
+        {
+            InputSecurities = new BindableCollection<string>();
+            Securities = new BindableCollection<Security>();
+            InputFields = new BindableCollection<string>();
+            Fields = new BindableCollection<string>();
+            HistoricalDataFrequencies = new BindableCollection<string>{
+                "Daily","Weekly","Monthly","Quarterly","Quarterly","SemiAnnual","Yearly"};
+
+            InitializeDefaults();
+        }
+
+        private void InitializeDefaults()
+        {
+            SelectedSecurity = null;
+            Sele
+            SelectedHistoricalDataFrequency = HistoricalDataFrequencies[0];
+        }
+
+        public BindableCollection<string> InputSecurities { get; private set; }
+        public BindableCollection<Security> Securities { get; private set; }
+        public BindableCollection<string> InputFields { get; private set; }
+        public BindableCollection<string> Fields { get; private set; }
+        public BindableCollection<string> HistoricalDataFrequencies { get; private set; }
+
+        public string SelectedInputSecurity { get; set; }
+        public Security SelectedSecurity { get; set; }
+        public string SelectedInputField { get; set; }
+        public string SelectedField { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public bool IsHistoricalRequest { get; set; }
+        public string SelectedHistoricalDataFrequency { get; set; }
+
+        public void Request()
         {
         }
     }
